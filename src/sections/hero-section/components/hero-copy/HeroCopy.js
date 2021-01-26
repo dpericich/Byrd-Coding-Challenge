726f64d485cfac11e480484f9da1bbfb9cb503e4
Daniel <dpericich@gmail.com>
2021-01-26 08:00:20 -0600

    commit changed files from hero merge

---
+++ src/sections/hero-section/components/hero-copy/HeroCopy.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./hero-copy.css";
+
+const HeroCopy = () => {
+  return (
+    <div className="hero-copy">
+      <h2 className="hero-copy__title">F-210</h2>
+      <div className="hero-copy__tagline">
+        "The fastest drone on the planet"
+      </div>
+      <div className="hero-copy__source">"The- The Wirecutter</div>
+    </div>
+  );
+};
+
+export default HeroCopy;
