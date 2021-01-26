45daeaf07aaae37821e6901989825c1fdb149b3f
Daniel <dpericich@gmail.com>
2021-01-25 23:02:38 -0600

    initial setup for directories, assets and fonts

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import logowhite from "../assets/logo-white.png";
+import shadow from "../assets/shadow.png";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="section1">Hello</div>
+      <div className="section2">World</div>
+    </div>
+  );
+}
+
+export default App;
